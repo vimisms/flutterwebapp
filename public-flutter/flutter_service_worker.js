013b7a85c84db7512edde94fabb5a82e39935fe9
Vivek Mishra <vimis@microsoft.com>
2024-04-01 19:09:07 +0530

    Update webview package

--- public-flutter/flutter_service_worker.js
+++ public-flutter/flutter_service_worker.js
@@ -8,7 +8,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "8317d9ddff2a40ff5d3b57c464a067f4",
-"assets/NOTICES": "267c3047c1a3b68b80b5d5298f3ae13c",
+"assets/NOTICES": "e1b51d20c08d8392bf1521429f9b64c4",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "89ed8f4e49bcdfc0b5bfc9b24591e347",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
@@ -27,9 +27,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a2d3a85508d431a70f1dbbde299d38a1",
-"/": "a2d3a85508d431a70f1dbbde299d38a1",
-"main.dart.js": "57ed83ebcecef3661ce31e37b29cf6ab",
+"index.html": "080b4bebef1f0affa050e860bbe21507",
+"/": "080b4bebef1f0affa050e860bbe21507",
+"main.dart.js": "f10b7bace4182df16680370dbd9eca45",
 "manifest.json": "faddd3c3d0030b5387506b1ed8c405e7",
 "version.json": "f7afe22fa2e97860b40ba932a7f5720e"};
 // The application shell files that are downloaded before a service worker can
